edc3e2fb7e453a6ddaaa3c6d82c9cc42d5c6c4f1
Dima Ilvokhin <d@ilvokhin.com>
2020-09-09 19:59:24 +0300

    Add solution to labyrinth problem

--- graph_algorithms/labyrinth.cpp
+++ graph_algorithms/labyrinth.cpp
@@ -30,14 +30,43 @@ bool operator == (const point& a, const point& b)
     return tie(a.x, a.y) == tie(b.x, b.y);
 }
 
-struct item {
-    point p;
-    string path;
-};
+bool operator < (const point& a, const point& b)
+{
+    return tie(a.x, a.y) < tie(b.x, b.y);
+}
 
 bool is_valid(const point& p, int n, int m)
 {
     return 0 <= p.x && p.x < n && 0 <= p.y && p.y < m;
+}
+
+string restore_path(const point& end, const point& begin,
+                    const map<point, point>& parent)
+{
+    string path;
+    point cur = end;
+
+    while (!(cur == begin)) {
+        auto it = parent.find(cur);
+        assert(it != parent.end());
+
+        point prev = it->second;
+        if (prev.x == cur.x && prev.y - 1 == cur.y)
+            path += 'L';
+        else if (prev.x - 1 == cur.x && prev.y == cur.y)
+            path += 'U';
+        else if (prev.x == cur.x && prev.y + 1 == cur.y)
+            path += 'R';
+        else if(prev.x + 1 == cur.x && prev.y == cur.y)
+            path += 'D';
+        else
+            assert(false);
+
+        cur = prev;
+    }
+
+    reverse(path.begin(), path.end());
+    return path;
 }
 
 bool go(const vector<string>& g, const point& begin, const point& end,
@@ -47,22 +76,21 @@ bool go(const vector<string>& g, const point& begin, const point& end,
     int m = g[0].size();
 
     vector<vector<bool> > used(n, vector<bool>(m));
+    map<point, point> parent;
 
     static int dx[] = {0, -1, 0, 1};
     static int dy[] = {-1, 0, 1, 0};
-    static char dir[] = "LURD";
 
-    queue<item> q;
-    q.push({ begin, "" });
+    queue<point> q;
+    q.push(begin);
     used[begin.x][begin.y] = true;
 
     while (!q.empty()) {
-        const item cur_item = q.front();
-        const point cur = cur_item.p;
+        const point cur = q.front();
         q.pop();
    
         if (cur == end) {
-            path = cur_item.path;
+            path = restore_path(cur, begin, parent);
             return true;
         }
 
@@ -76,7 +104,8 @@ bool go(const vector<string>& g, const point& begin, const point& end,
                 continue;
 
             used[next.x][next.y] = true;
-            q.push({ next, cur_item.path + dir[k] });
+            parent[next] = cur;
+            q.push(next);
         }
     }
 
